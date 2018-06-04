041cba68891b67b38633e0e45fc54d095a8f8953
Andrey Babukov <andrey.babukov@abcsolutions.pro>
2018-06-04 09:05:49 +0300

    Initial commit

---
+++ AVDSLabs.TimeBoss2/AVDSLabs.TimeBoss2.TaskBoss/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AVDSLabs.TimeBoss2.TaskBoss
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AVDSLabs.TimeBoss2/AVDSLabs.TimeBoss2.TaskBoss/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AVDSLabs.TimeBoss2.TaskBoss.Startup))]
+namespace AVDSLabs.TimeBoss2.TaskBoss
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
